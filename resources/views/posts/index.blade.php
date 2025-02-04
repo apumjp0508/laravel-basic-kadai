9984878ba6c654b4178c82218afaf79ec81b875b
apumjp0508 <akihisatenshan@gmail.com>
2025-02-05 07:42:15 +0900

    3回目課題

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -7,5 +7,23 @@
 </head>
 <body>
     <h1>投稿一覧</h1>
+    <table>
+        <tr>
+            <th>id</th>
+            <th>title</th>
+            <th>content</th>
+            <th>createdate</th>
+            <th>updatedate</th>
+        </tr>
+        @foreach($posts as $post)
+           <tr>
+               <td>{{ $post->id }}</td>
+               <td>{{ $post->title }}</td>
+               <td>{{ $post->content }}</td>
+               <td>{{ $post->created_at }}</td>
+               <td>{{ $post->updated_at }}</td>
+           </tr>            
+       @endforeach  
+    </table>
 </body>
 </html>
