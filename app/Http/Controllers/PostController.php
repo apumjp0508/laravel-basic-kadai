30ab29dee14fc17852fd8c5bf2b78c2ac2fdbdb5
apumjp0508 <akihisatenshan@gmail.com>
2025-02-06 07:21:18 +0900

    4回目課題

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -3,8 +3,8 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-
 use Illuminate\Support\Facades\DB;
+use App\Models\Post;
 
 class PostController extends Controller
 {
@@ -13,6 +13,14 @@ class PostController extends Controller
         $posts=DB::table('posts')->get();
 
          return view('posts/index',compact('posts'));
+         //viewの引数にはviewファイルのパス名を書く
+    }
+
+    public function show($id) {
+
+        $post=Post::find($id);
+
+         return view('posts/show',compact('post'));
          //viewの引数にはviewファイルのパス名を書く
     }
 }
